5c9c0d43fc6249b743c00147f8808424ce5cad60
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-24 17:33:59 -0500

    added gitignore file

---
+++ TrackZilla/src/main/java/com/tobias/saul/TrackZilla/TrackZillaApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.TrackZilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackZillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackZillaApplication.class, args);
+	}
+
+}
